2d2e01f98d957f26e1bd5e427ccb32a210b494fc
Ryan <ryan.y.chung@gmail.com>
2016-09-21 17:10:09 -0400

    signing up as a new user gets saved

--- awesome-fund/app/controllers/users_controller.rb
+++ awesome-fund/app/controllers/users_controller.rb
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render "new"
+    end
   end
 
   def update
