0f4237da6df218a2e7020b906d0c17be22001742
Shane <shanebeehler@me.com>
2016-09-23 09:36:56 -0400

    removed a space. formatting

--- awesome-fund/app/models/reward.rb
+++ awesome-fund/app/models/reward.rb
@@ -1,6 +1,6 @@
 class Reward < ApplicationRecord
   belongs_to :project
 
-  validates  :description, presence: true
+  validates :description, presence: true
   validates :benchmark, numericality: { only_integer: true }
 end
