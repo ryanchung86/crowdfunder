42f8871b3f8fbd3b450bf41f79b37941a26c92ff
Shane <shanebeehler@me.com>
2016-09-23 13:36:23 -0400

    rewards visible on project show page

--- awesome-fund/app/controllers/projects_controller.rb
+++ awesome-fund/app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+
   end
 
   def show
     @project = Project.find(params[:id])
-
+    @rewards = @project.rewards
 
     if current_user
       @pledge = @project.pledges.build
